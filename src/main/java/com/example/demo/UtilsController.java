b7f9c72221998cd028a4e97dd70172a9a870c3ff
pawel <pawel.hubisz.98@wp.pl>
2022-06-16 11:59:36 +0200

    review fix (added enum)

--- src/main/java/com/example/demo/UtilsController.java
+++ src/main/java/com/example/demo/UtilsController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.enums.ApplicationStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,6 @@ public class UtilsController {
 
     @GetMapping("/status")
     public String status() {
-        return "UP";
+        return ApplicationStatus.UP.name();
     }
 }
